db4a371c14d3272984a32136acce8c96ff15649c
Baraev Sergey <Baraev.sergey@mail.ru>
2020-03-26 01:40:20 +0500

    добавил реакцию на кнопку добавить

--- app/src/main/java/com/example/password_manager/List_of_passwords.java
+++ app/src/main/java/com/example/password_manager/List_of_passwords.java
@@ -3,14 +3,27 @@ package com.example.password_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class List_of_passwords extends AppCompatActivity {
-
+    Button add;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_passwords);
+        //находим view элементы
+        add = (Button) findViewById(R.id.add);
+        //создание обрабочтка нажатия
+        View.OnClickListener clickadd = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Test Click", Toast.LENGTH_SHORT).show();
+            }
+        };
+        add.setOnClickListener(clickadd);
     }
     private static long back_pressed;
     @Override
@@ -24,4 +37,5 @@ public class List_of_passwords extends AppCompatActivity {
         }
         back_pressed = System.currentTimeMillis();
     }
+
 }
