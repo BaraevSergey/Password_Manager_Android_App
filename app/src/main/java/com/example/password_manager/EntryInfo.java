757260b017dc3397655db00d51acf64fe7b27372
Baraev Sergey <Baraev.sergey@mail.ru>
2020-03-29 01:06:03 +0500

    Добавил обработчик кнопки отмена

--- app/src/main/java/com/example/password_manager/EntryInfo.java
+++ app/src/main/java/com/example/password_manager/EntryInfo.java
@@ -3,12 +3,25 @@ package com.example.password_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EntryInfo extends AppCompatActivity {
-
+    Button canc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_info);
+        canc = (Button) findViewById(R.id.cancel);
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                finish();
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        canc.setOnClickListener(oclBtnOk);
     }
 }
