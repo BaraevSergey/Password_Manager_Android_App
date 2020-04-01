2e597ae04ba51357b172edde7416538514832842
Baraev Sergey <Baraev.sergey@mail.ru>
2020-04-01 18:51:17 +0500

    Добавил проверку правильности введенных данных

--- app/src/main/java/com/example/password_manager/RegisterPage.java
+++ app/src/main/java/com/example/password_manager/RegisterPage.java
@@ -37,7 +37,11 @@ public class RegisterPage extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (Login.getText().toString().length() != 0 && Password.getText().toString().length() != 0 && ConfirmPassword.getText().toString().length() != 0) {
-                    Toast.makeText(RegisterPage.this, "Succesful", Toast.LENGTH_SHORT).show();
+                    Zareg.setBackgroundColor(getResources().getColor(R.color.RegistrActive));
+                }
+                else
+                {
+                    Zareg.setBackgroundColor(getResources().getColor(R.color.RegistrNonActive));
                 }
             }
         };
@@ -56,9 +60,40 @@ public class RegisterPage extends AppCompatActivity {
                 //4) Логин >=6 символов
                 //5) Пароль >= 8 символов
                 //6) Пароль и подтверждение совпадают
-                //7)
+                //7) Содержат только разрешенные символы
+                if (Login.getText().toString().length() != 0 && Password.getText().toString().length() != 0 && ConfirmPassword.getText().toString().length() != 0) { //проверка пустоты
+                    if (Login.getText().toString().length() >= 6) { //проверка длины логина
+                        if (Password.getText().toString().length() >= 8) { //проверка длины пароля
+                            if (Password.getText().toString() == ConfirmPassword.getText().toString()) {
+                                String RegEx = "[0-9A-Za-zА-Яа-я]{0,}";
+                                if (Login.getText().toString().matches(RegEx) || Password.getText().toString().matches(RegEx)) {
+                                    //здесь можно программку писать, ееееее
+                                }
+                                else {
+                                    String MessageError = "Логин или пароль содержат недопустимые символы, используйте только [0-9],[a-z],[А-Я]";
+                                    Toast.makeText(RegisterPage.this, MessageError, Toast.LENGTH_SHORT).show();
+                                }
 
+                            }
+                            else {
+                                Toast.makeText(RegisterPage.this, "Пароли не совпадают", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(RegisterPage.this, "Длина пароля должна быть больше 7 символов", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(RegisterPage.this, "Длина логина должна быть больше 5 символов", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(RegisterPage.this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+                }
             }
+
+
         };
         Zareg.setOnClickListener(ButtonClick);
 
