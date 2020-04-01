f299b64b2480ce0270ea82223a581e4a4de0e120
Baraev Sergey <Baraev.sergey@mail.ru>
2020-04-01 19:43:27 +0500

    Выделил проверку в метод

--- app/src/main/java/com/example/password_manager/RegisterPage.java
+++ app/src/main/java/com/example/password_manager/RegisterPage.java
@@ -22,7 +22,7 @@ public class RegisterPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
         Zareg = (Button) findViewById(R.id.Register);
-        Login = (EditText) findViewById(R.id.LoginInfo) ;
+        Login = (EditText) findViewById(R.id.LoginInfo);
         Password = (EditText) findViewById(R.id.PasswordEdit);
         ConfirmPassword = (EditText) findViewById(R.id.ConfirmPassword);
         TextWatcher check = new TextWatcher() {
@@ -38,16 +38,14 @@ public class RegisterPage extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (Login.getText().toString().length() != 0 && Password.getText().toString().length() != 0 && ConfirmPassword.getText().toString().length() != 0) {
                     Zareg.setBackgroundColor(getResources().getColor(R.color.RegistrActive));
-                }
-                else
-                {
+                } else {
                     Zareg.setBackgroundColor(getResources().getColor(R.color.RegistrNonActive));
                 }
             }
         };
-        Login.addTextChangedListener(check) ;
-        Password.addTextChangedListener(check) ;
-        ConfirmPassword.addTextChangedListener(check) ;
+        Login.addTextChangedListener(check);
+        Password.addTextChangedListener(check);
+        ConfirmPassword.addTextChangedListener(check);
 
 
         View.OnClickListener ButtonClick = new View.OnClickListener() {
@@ -61,41 +59,59 @@ public class RegisterPage extends AppCompatActivity {
                 //5) Пароль >= 8 символов
                 //6) Пароль и подтверждение совпадают
                 //7) Содержат только разрешенные символы
-                if (Login.getText().toString().length() != 0 && Password.getText().toString().length() != 0 && ConfirmPassword.getText().toString().length() != 0) { //проверка пустоты
-                    if (Login.getText().toString().length() >= 6) { //проверка длины логина
-                        if (Password.getText().toString().length() >= 8) { //проверка длины пароля
-                            if (Password.getText().toString() == ConfirmPassword.getText().toString()) {
-                                String RegEx = "[0-9A-Za-zА-Яа-я]{0,}";
-                                if (Login.getText().toString().matches(RegEx) || Password.getText().toString().matches(RegEx)) {
-                                    //здесь можно программку писать, ееееее
-                                }
-                                else {
-                                    String MessageError = "Логин или пароль содержат недопустимые символы, используйте только [0-9],[a-z],[А-Я]";
-                                    Toast.makeText(RegisterPage.this, MessageError, Toast.LENGTH_SHORT).show();
-                                }
+                String LoginEnrty = Login.getText().toString();
+                String PasswordEntry = Password.getText().toString();
+                String ConfirmPasswordEntry = ConfirmPassword.getText().toString();
+                if  (checking(LoginEnrty, PasswordEntry, ConfirmPasswordEntry) == true) {
+                    Toast.makeText(RegisterPage.this, "Норм", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(RegisterPage.this, "Не норм", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
+
+        Zareg.setOnClickListener(ButtonClick);
+
+        };
+
+    public boolean checking(String Lgn, String Psw, String CnfPsw) {
+        if (Lgn.length() != 0 && Psw.length() != 0 && CnfPsw.length() != 0) { //проверка пустоты
+                if (Lgn.length() >= 6) { //проверка длины логина
+                    if (Psw.length() >= 8) { //проверка длины пароля
+                        if (Psw.equals(CnfPsw)) { //проверка совпадают ли пароли
+                            String RegEx = "[0-9A-Za-zА-Яа-я]{0,}";
+                            if (Lgn.matches(RegEx) || Psw.matches(RegEx)) { //проверка на недопустимые символы
+                                return true;
                             }
                             else {
-                                Toast.makeText(RegisterPage.this, "Пароли не совпадают", Toast.LENGTH_SHORT).show();
+                                String MessageError = "Логин или пароль содержат недопустимые символы, используйте только [0-9],[a-z],[А-Я]";
+                                Toast.makeText(RegisterPage.this, MessageError, Toast.LENGTH_SHORT).show();
+                                return false;
                             }
+
                         }
                         else {
-                            Toast.makeText(RegisterPage.this, "Длина пароля должна быть больше 7 символов", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterPage.this, "Пароли не совпадают", Toast.LENGTH_SHORT).show();
+                            return false;
                         }
                     }
                     else {
-                        Toast.makeText(RegisterPage.this, "Длина логина должна быть больше 5 символов", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegisterPage.this, "Длина пароля должна быть больше 7 символов", Toast.LENGTH_SHORT).show();
+                        return false;
                     }
                 }
-                else
-                {
-                    Toast.makeText(RegisterPage.this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(RegisterPage.this, "Длина логина должна быть больше 5 символов", Toast.LENGTH_SHORT).show();
+                    return false;
                 }
             }
-
-
-        };
-        Zareg.setOnClickListener(ButtonClick);
+            else
+            {
+                Toast.makeText(RegisterPage.this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
 
     }
-}
